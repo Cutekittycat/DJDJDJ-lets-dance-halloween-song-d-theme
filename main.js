d12fd51efadde342f8e2ae42acfe4a6f751fa141
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2021-10-24 15:30:46 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload()
 {
     song = loadSound("music.mp3");
@@ -11,6 +17,28 @@ function setup()
    
    video= createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialized');
 }
 
 function draw(){
@@ -20,4 +48,6 @@ function draw(){
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
